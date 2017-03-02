367cd306e87c25b52a3b5a7c6de846be4762d72b
Chris Rabuse <rab233@live.com>
2017-03-01 19:03:55 -0600

    Update demo

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -278,7 +278,7 @@ var router = new _alpineRouter2.default([{
     template: '<h1>Parent Route</h1><p>Child routes will be rendered below!</p><router-outlet></router-outlet>',
     children: [{
         path: 'child',
-        template: '<h2>Child</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas iaculis leo quis viverra. Duis sodales nulla sem, vitae congue sapien luctus eget. Sed feugiat aliquam dapibus. Cras elementum, leo sed laoreet pulvinar, nunc nulla auctor lectus, in blandit est mauris ut sem. Sed a nibh urna. Etiam eget aliquam augue. Nunc id ornare justo, in finibus nibh. Nunc mattis, metus nec volutpat tempus, nisl sem sodales dolor, nec tincidunt ipsum mauris nec ex.</p>'
+        template: '<h2>Child Route</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas iaculis leo quis viverra. Duis sodales nulla sem, vitae congue sapien luctus eget. Sed feugiat aliquam dapibus. Cras elementum, leo sed laoreet pulvinar, nunc nulla auctor lectus, in blandit est mauris ut sem. Sed a nibh urna. Etiam eget aliquam augue. Nunc id ornare justo, in finibus nibh. Nunc mattis, metus nec volutpat tempus, nisl sem sodales dolor, nec tincidunt ipsum mauris nec ex.</p>'
     }]
 }, {
     path: 'external',
